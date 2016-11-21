343059fa3b41670109d0162495055b346fcf9bd3
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-11-21 13:03:02 +0000

    Add design view model

---
+++ OBD2Reader/OBD2Reader/ViewModel/DesignerViewModel/DesignerMainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using OBD2Reader.Model;
+
+namespace OBD2Reader.ViewModel.DesignerViewModel
+{
+    public class DesignerMainWindowViewModel
+    {
+        public CarModel Car => new CarModel("Vauxhall", "Corsa");
+    }
+}
