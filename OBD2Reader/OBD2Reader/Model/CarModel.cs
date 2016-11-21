a15e4afaff39a76abd414befc4cc92455f1cd381
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-11-21 12:57:02 +0000

    Display make and model of car

--- OBD2Reader/OBD2Reader/Model/CarModel.cs
+++ OBD2Reader/OBD2Reader/Model/CarModel.cs
@@ -2,5 +2,33 @@
 {
     public class CarModel : BaseModel
     {
+        private string _make;
+        private string _model;
+
+        public CarModel(string make, string model)
+        {
+            Make = make;
+            Model = model;
+        }
+
+        public string Make
+        {
+            get { return _make; }
+            set
+            {
+                _make = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set
+            {
+                _model = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
--- OBD2Reader/OBD2Reader/ViewModel/MainWindowViewModel.cs
+++ OBD2Reader/OBD2Reader/ViewModel/MainWindowViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace OBD2Reader.ViewModel
+﻿using OBD2Reader.Model;
+
+namespace OBD2Reader.ViewModel
 {
     public class MainWindowViewModel : BaseViewModel
     {
+        private CarModel _car;
+
+        public CarModel Car
+        {
+            get { return _car; }
+            set
+            {
+                _car = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
