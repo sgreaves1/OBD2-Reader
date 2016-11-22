af28f85c7062091306da1fb40b903f3b46b6a3fa
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-11-22 18:10:03 +0000

    Add relay command class

---
+++ OBD2Reader/OBD2Reader/Command/RelayCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace OBD2Reader.Command
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        private readonly Action _methodToExecute;
+        private readonly Func<bool> _canExecuteEvaluator;
+        public RelayCommand(Action methodToExecute, Func<bool> canExecuteEvaluator)
+        {
+            _methodToExecute = methodToExecute;
+            _canExecuteEvaluator = canExecuteEvaluator;
+        }
+        public RelayCommand(Action methodToExecute)
+            : this(methodToExecute, null)
+        {
+        }
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecuteEvaluator == null)
+            {
+                return true;
+            }
+            
+            bool result = _canExecuteEvaluator.Invoke();
+            return result;
+            
+        }
+        public void Execute(object parameter)
+        {
+            _methodToExecute.Invoke();
+        }
+    }
+}
