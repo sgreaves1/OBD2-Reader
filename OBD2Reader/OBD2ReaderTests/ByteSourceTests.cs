1fdcd513ba439230e9233a6503cfff86ed928484
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-11-22 13:03:34 +0000

    Add test byte source to test project

--- OBD2Reader/OBD2ReaderTests/ByteSourceTests.cs
+++ OBD2Reader/OBD2ReaderTests/ByteSourceTests.cs
@@ -8,6 +8,7 @@ namespace OBD2ReaderTests
         [TestMethod]
         public void TestMethod1()
         {
+            
         }
     }
 }
---
+++ OBD2Reader/OBD2ReaderTests/TestByteSource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OBD2Reader.Model.DataReceiver;
+
+namespace OBD2ReaderTests
+{
+    public class TestByteSource : IByteSource
+    {
+        public int Read(byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler DataReceived;
+    }
+}
