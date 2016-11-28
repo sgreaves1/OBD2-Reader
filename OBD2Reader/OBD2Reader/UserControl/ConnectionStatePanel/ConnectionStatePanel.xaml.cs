ae1fb4114318171fe51773ebbb0e4271424da301
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-11-28 15:08:13 +0000

    Connection status icons now displayed from status uc

--- OBD2Reader/OBD2Reader/UserControl/ConnectionStatePanel/ConnectionStatePanel.xaml.cs
+++ OBD2Reader/OBD2Reader/UserControl/ConnectionStatePanel/ConnectionStatePanel.xaml.cs
@@ -1,13 +1,31 @@
-﻿namespace OBD2Reader.UserControl.ConnectionStatePanel
+﻿using System.Windows;
+
+namespace OBD2Reader.UserControl.ConnectionStatePanel
 {
     /// <summary>
     /// Interaction logic for ConnectionStatePanel.xaml
     /// </summary>
     public partial class ConnectionStatePanel 
     {
+        // Using a DependencyProperty as the backing store for IsConnected.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsConnectedProperty =
+            DependencyProperty.Register("IsConnected", 
+                typeof(bool), 
+                typeof(ConnectionStatePanel), 
+                new PropertyMetadata(false));
+        
         public ConnectionStatePanel()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsConnected
+        {
+            get { return (bool)GetValue(IsConnectedProperty); }
+            set { SetValue(IsConnectedProperty, value); }
         }
     }
 }
